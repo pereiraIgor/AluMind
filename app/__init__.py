3f6ee83a28f5aa2597cc9568a48ad4ace3928baa
Igor <igor.pereira@ccc.ufcg.edu.br>
2025-03-31 21:33:39 -0300

    Disponibilizando app via docker

--- app/__init__.py
+++ app/__init__.py
@@ -17,6 +17,8 @@ def create_app():
     init_scheduler(app)
 
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
     
     from app.routes import bp
     app.register_blueprint(bp)
